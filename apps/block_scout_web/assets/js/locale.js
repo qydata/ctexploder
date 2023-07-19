96313fc752ea356af48abf266ada7545fe3e76e8
iozhaq <iozhaq@outlook.com>
2023-07-19 10:47:00 +0800

    公安备案添加

---
+++ apps/block_scout_web/assets/js/locale.js
@@ -0,0 +1,10 @@
+import moment from 'moment'
+require('moment/locale/zh-cn')
+import numeral from 'numeral'
+import 'numeral/locales'
+
+// export const locale = 'en'
+export const locale = 'en'  // 新标准简体中文
+
+// moment.locale(locale)
+numeral.locale(locale)
